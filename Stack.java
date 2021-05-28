d8252b99fd138191dd9c897a2d6fce0f4ddc818a
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2021-05-28 08:44:45 -0300

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,36 @@
+
+package br.unicap.ed.TAD;
+
+public class Stack<T> {
+    private StackNode<T> topo;
+    
+    public boolean isEmpty(){ // verifica se a pilha está vazia
+        if (topo == null) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean isFull(){ // verifica se a pilha está cheia (abstrato)
+        return false;
+    }
+    
+    public void push(T valor){ //insere valor no topo da pilha
+        StackNode<T> novo = new StackNode(valor);
+        if (isEmpty()) {
+            
+        } else {
+            novo.setProx(topo);
+            topo = novo;
+        }
+    }
+    
+    public T pop(){  //retira e retorna valor do topo da pilha
+        StackNode<T> aux = topo;
+        topo = topo.getProx();
+        
+        return aux.getInfo();
+    }
+    
+}
