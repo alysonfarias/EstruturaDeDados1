b677e47f3095a4b962b02864c673a9273f9d06c0
Alyson Farias <63425960+alysonfarias@users.noreply.github.com>
2021-05-29 19:30:59 -0300

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,36 +1,39 @@
-
 package br.unicap.ed.TAD;
 
 public class Stack<T> {
+
     private StackNode<T> topo;
-    
-    public boolean isEmpty(){ // verifica se a pilha está vazia
+
+    public boolean isEmpty() { // verifica se a pilha está vazia
         if (topo == null) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
-    public boolean isFull(){ // verifica se a pilha está cheia (abstrato)
+
+    public boolean isFull() { // verifica se a pilha está cheia (abstrato)
         return false;
     }
-    
-    public void push(T valor){ //insere valor no topo da pilha
+
+    public void push(T valor) { //insere valor no topo da pilha
         StackNode<T> novo = new StackNode(valor);
         if (isEmpty()) {
-            
+            topo = novo;
         } else {
             novo.setProx(topo);
             topo = novo;
         }
     }
-    
-    public T pop(){  //retira e retorna valor do topo da pilha
-        StackNode<T> aux = topo;
+
+    public T pop() {  // desempilhar
+        T valor = topo.getInfo();
         topo = topo.getProx();
-        
-        return aux.getInfo();
+        return valor;
     }
-    
+
+    public T top() { // informa o topo
+        return topo.getInfo();
+    }
+
 }
